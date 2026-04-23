b2e407266aae42b11ab77571693c4b11cf7dde0f
KaranMahiya <karanmahiya21@gmail.com>
2026-04-23 15:28:42 +0000

    git add . git commit -m "added home route" git push

--- server.py
+++ server.py
@@ -77,3 +77,7 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 10000))  # 🔥 FIX
     app.run(host="0.0.0.0", port=port)
+
+@app.route("/")
+def home():
+    return "Server is running 🚀"
